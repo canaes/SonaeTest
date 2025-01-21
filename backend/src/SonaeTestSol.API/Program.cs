980a32ac60ba7b185a527095ec3d8299b5050034
Guilherme Canaes <canaesg@gmail.com>
2025-01-20 22:17:35 -0300

    Add Exception filter

--- backend/src/SonaeTestSol.API/Program.cs
+++ backend/src/SonaeTestSol.API/Program.cs
@@ -1,4 +1,5 @@
 using SonaeTestSol.API.Configuration;
+using SonaeTestSol.API.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,11 @@ builder.Services.ResolveDependencies();
 builder.Services.AddApiVersioningConfiguration();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers(x =>
+{
+    x.Filters.Add<HttpResponseExceptionFilter>();
+});
 
 var app = builder.Build();
 
